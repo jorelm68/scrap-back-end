dc548a6de0eac08e6252e6b520b9a28c4bdc02eb
jorelm68 <methan@umich.edu>
2023-12-01 19:13:39 -0500

    Removed scrap threading functions

--- routes/scraps.js
+++ routes/scraps.js
@@ -3,8 +3,6 @@ const {
     exists,
     saveScrap,
     deleteScraps,
-    addThread,
-    removeThread,
 } = require('../controllers/scrapController')
 
 const router = express.Router()
@@ -12,7 +10,5 @@ const router = express.Router()
 router.post('/exists', exists)
 router.post('/saveScrap', saveScrap)
 router.post('/deleteScraps', deleteScraps)
-router.patch('/addThread', addThread)
-router.patch('/removeThread', removeThread)
 
 module.exports = router
