9ebc00d56e60f345e411478c2940242fbb0a9a05
jorelm68 <methan@umich.edu>
2024-02-26 10:29:09 -0500

    Implemented the website!

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -117,8 +117,82 @@ const notificationIcon = async (req, res) => {
     res.send(buffer)
 }
 
-const profileBooksIcon = async (req, res) => {
+const profileBooksImage = async (req, res) => {
     const buffer = fs.readFileSync('assets/photos/profileBooks.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+
+const libraryScrapsImage = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/libraryScraps.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+
+const book1Image = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/book1.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+const book2Image = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/book2.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+const book3Image = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/book3.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+const book4Image = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/book4.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+const book5Image = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/book5.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+const book6Image = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/book6.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+const book7Image = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/book7.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+const book8Image = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/book8.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+const book9Image = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/book9.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+const book10Image = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/book10.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+
+const cameraPrograph = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/cameraPrograph.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+const cameraRetrograph = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/cameraRetrograph.jpg')
+    res.setHeader('Content-Type', 'image/jpg')
+    res.send(buffer)
+}
+
+const profileMap = async (req, res) => {
+    const buffer = fs.readFileSync('assets/photos/profileMap.jpg')
     res.setHeader('Content-Type', 'image/jpg')
     res.send(buffer)
 }
@@ -131,5 +205,19 @@ module.exports = {
     changeEmail,
     homePage,
     notificationIcon,
-    profileBooksIcon,
+    profileBooksImage,
+    libraryScrapsImage,
+    book1Image,
+    book2Image,
+    book3Image,
+    book4Image,
+    book5Image,
+    book6Image,
+    book7Image,
+    book8Image,
+    book9Image,
+    book10Image,
+    cameraPrograph,
+    cameraRetrograph,
+    profileMap,
 }
--- routes/website.js
+++ routes/website.js
@@ -8,7 +8,21 @@ const {
     activateAccount,
     changeEmail,
     notificationIcon,
-    profileBooksIcon,
+    profileBooksImage,
+    libraryScrapsImage,
+    book1Image,
+    book2Image,
+    book3Image,
+    book4Image,
+    book5Image,
+    book6Image,
+    book7Image,
+    book8Image,
+    book9Image,
+    book10Image,
+    cameraPrograph,
+    cameraRetrograph,
+    profileMap,
 } = require('../controllers/viewsController')
 
 const router = express.Router()
@@ -20,6 +34,19 @@ router.post('/resetPasswordConfirmation', resetPasswordConfirmation)
 router.get('/activateAccount/:confirmationToken', activateAccount)
 router.get('/changeEmail/:confirmationToken', changeEmail)
 router.get('/notificationIcon', notificationIcon)
-router.get('/profileBooksIcon', profileBooksIcon)
-
+router.get('/profileBooksImage', profileBooksImage)
+router.get('/libraryScrapsImage', libraryScrapsImage)
+router.get('/book1Image', book1Image),
+router.get('/book2Image', book2Image),
+router.get('/book3Image', book3Image),
+router.get('/book4Image', book4Image),
+router.get('/book5Image', book5Image),
+router.get('/book6Image', book6Image),
+router.get('/book7Image', book7Image),
+router.get('/book8Image', book8Image),
+router.get('/book9Image', book9Image),
+router.get('/book10Image', book10Image),
+router.get('/cameraPrograph', cameraPrograph),
+router.get('/cameraRetrograph', cameraRetrograph),
+router.get('/profileMap', profileMap),
 module.exports = router
